a5ab0364abffbf52d36eca9e0d7354a94c7c1cf8
fawwasaldy <fawwas.aldy2111@gmail.com>
2025-06-17 22:43:06 +0700

    feat: add soft delete to the timestamp

--- infrastructure/database/refresh_token/schema.go
+++ infrastructure/database/refresh_token/schema.go
@@ -5,19 +5,27 @@ import (
 	"fp-kpl/domain/refresh_token"
 	"fp-kpl/domain/shared"
 	"github.com/google/uuid"
+	"gorm.io/gorm"
 	"time"
 )
 
 type RefreshToken struct {
-	ID        uuid.UUID `gorm:"primaryKey;type:uuid;default:uuid_generate_v4();column:id"`
-	UserID    uuid.UUID `gorm:"type:uuid;not null;column:user_id"`
-	Token     string    `gorm:"type:varchar(255);not null;uniqueIndex;column:token"`
-	ExpiresAt time.Time `gorm:"type:timestamp with time zone;not null;column:expires_at"`
-	CreatedAt time.Time `gorm:"type:timestamp with time zone;column:created_at"`
-	UpdatedAt time.Time `gorm:"type:timestamp with time zone;column:updated_at"`
+	ID        uuid.UUID      `gorm:"primaryKey;type:uuid;default:uuid_generate_v4();column:id"`
+	UserID    uuid.UUID      `gorm:"type:uuid;not null;column:user_id"`
+	Token     string         `gorm:"type:varchar(255);not null;uniqueIndex;column:token"`
+	ExpiresAt time.Time      `gorm:"type:timestamp with time zone;not null;column:expires_at"`
+	CreatedAt time.Time      `gorm:"type:timestamp with time zone;column:created_at"`
+	UpdatedAt time.Time      `gorm:"type:timestamp with time zone;column:updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"type:timestamp with time zone;column:deleted_at"`
 }
 
 func EntityToSchema(entity refresh_token.RefreshToken) RefreshToken {
+	var deletedAtTime time.Time
+	if entity.DeletedAt != nil {
+		deletedAtTime = *entity.DeletedAt
+	} else {
+		deletedAtTime = time.Time{}
+	}
 	return RefreshToken{
 		ID:        entity.ID.ID,
 		UserID:    entity.UserID.ID,
@@ -25,6 +33,10 @@ func EntityToSchema(entity refresh_token.RefreshToken) RefreshToken {
 		ExpiresAt: entity.ExpiresAt,
 		CreatedAt: entity.CreatedAt,
 		UpdatedAt: entity.UpdatedAt,
+		DeletedAt: gorm.DeletedAt{
+			Time:  deletedAtTime,
+			Valid: entity.DeletedAt != nil,
+		},
 	}
 }
 
@@ -37,6 +49,7 @@ func SchemaToEntity(schema RefreshToken) refresh_token.RefreshToken {
 		Timestamp: shared.Timestamp{
 			CreatedAt: schema.CreatedAt,
 			UpdatedAt: schema.UpdatedAt,
+			DeletedAt: &schema.DeletedAt.Time,
 		},
 	}
 }
