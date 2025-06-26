407db2b0aa8242caee484fcd69edb28359c21a11
Ainun Nadhifah Syamsiyah <ainunsyamsiyah@gmail.com>
2025-06-26 23:34:51 +0700

    feat: implement table service and controller with routes for retrieving tables

--- domain/table/repository.go
+++ domain/table/repository.go
@@ -4,7 +4,7 @@ import "context"
 
 type (
 	Repository interface {
-		GetAllTable(ctx context.Context, tx interface{}) ([]Table, error)
+		GetAllTables(ctx context.Context, tx interface{}) ([]Table, error)
 		GetTableByID(ctx context.Context, tx interface{}, id string) (Table, error)
 	}
 )
