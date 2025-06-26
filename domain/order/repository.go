207e564f93c26985f6ebbbfa0ee835d29f167537
Ainun Nadhifah Syamsiyah <ainunsyamsiyah@gmail.com>
2025-06-26 11:48:01 +0700

    feat: add domain order

---
+++ domain/order/repository.go
@@ -0,0 +1,13 @@
+package order
+
+import "context"
+
+type (
+	Repository interface {
+		CreateOrder(ctx context.Context, tx interface{}, order Order) (Order, error)
+		GetAllOrder(ctx context.Context, tx interface{}) ([]Order, error)
+		GetOrderByID(ctx context.Context, tx interface{}, id string) (Order, error)
+		GetOrderByTransactionID(ctx context.Context, tx interface{}, transactionID string) ([]Order, error)
+		UpdateOrder(ctx context.Context, tx interface{}, order Order) (Order, error)
+	}
+)
