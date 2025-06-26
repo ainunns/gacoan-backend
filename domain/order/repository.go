5c34b62c868ecb885cca12bfab30da38cb66397a
Ainun Nadhifah Syamsiyah <ainunsyamsiyah@gmail.com>
2025-06-26 13:43:45 +0700

    refactor: rename order parameter in order repository for better clarity

--- domain/order/repository.go
+++ domain/order/repository.go
@@ -4,10 +4,10 @@ import "context"
 
 type (
 	Repository interface {
-		CreateOrder(ctx context.Context, tx interface{}, order Order) (Order, error)
+		CreateOrder(ctx context.Context, tx interface{}, orderEntity Order) (Order, error)
 		GetAllOrder(ctx context.Context, tx interface{}) ([]Order, error)
 		GetOrderByID(ctx context.Context, tx interface{}, id string) (Order, error)
 		GetOrderByTransactionID(ctx context.Context, tx interface{}, transactionID string) ([]Order, error)
-		UpdateOrder(ctx context.Context, tx interface{}, order Order) (Order, error)
+		UpdateOrder(ctx context.Context, tx interface{}, orderEntity Order) (Order, error)
 	}
 )
