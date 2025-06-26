f6c67526975428d03b3558db29e1264e4122cfb3
Ainun Nadhifah Syamsiyah <ainunsyamsiyah@gmail.com>
2025-06-26 20:11:37 +0700

    feat: add table migration and seeding functionality

--- infrastructure/database/migration/seeder.go
+++ infrastructure/database/migration/seeder.go
@@ -1,10 +1,15 @@
 package migration
 
 import (
+	"fp-kpl/infrastructure/database/migration/seed"
+
 	"gorm.io/gorm"
 )
 
 func Seeder(db *gorm.DB) error {
+	if err := seed.Table(db); err != nil {
+		return err
+	}
 
 	return nil
 }
