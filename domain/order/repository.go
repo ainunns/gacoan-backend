79146e9c6b43c67db639cfdd8aa7f4c2ff5d14e6
Ainun Nadhifah Syamsiyah <ainunsyamsiyah@gmail.com>
2025-06-28 18:03:44 +0700

    feat: implement order repository with methods for creating orders and retrieving orders by transaction ID

--- domain/order/repository.go
+++ domain/order/repository.go
@@ -5,9 +5,6 @@ import "context"
 type (
 	Repository interface {
 		CreateOrder(ctx context.Context, tx interface{}, orderEntity Order) (Order, error)
-		GetAllOrder(ctx context.Context, tx interface{}) ([]Order, error)
-		GetOrderByID(ctx context.Context, tx interface{}, id string) (Order, error)
 		GetOrderByTransactionID(ctx context.Context, tx interface{}, transactionID string) ([]Order, error)
-		UpdateOrder(ctx context.Context, tx interface{}, orderEntity Order) (Order, error)
 	}
 )
