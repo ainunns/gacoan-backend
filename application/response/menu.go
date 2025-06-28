2b807540318ef3bab48d7ab3e027dfccbf2389d7
Ainun Nadhifah Syamsiyah <ainunsyamsiyah@gmail.com>
2025-06-28 19:50:56 +0700

    feat: add image URL and availability fields to menu response and service methods

--- application/response/menu.go
+++ application/response/menu.go
@@ -7,6 +7,8 @@ type (
 		ID          string          `json:"id"`
 		Name        string          `json:"name"`
 		Description string          `json:"description"`
+		ImageUrl    string          `json:"image_url"`
+		IsAvailable bool            `json:"is_available"`
 		Price       decimal.Decimal `json:"price"`
 		Category    Category        `json:"category"`
 	}
