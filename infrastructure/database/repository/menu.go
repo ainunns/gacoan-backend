782172c6c21dc39b2edd704f1bc6277f22f20963
Ainun Nadhifah Syamsiyah <ainunsyamsiyah@gmail.com>
2025-06-28 20:16:11 +0700

    feat: implement update menu availability functionality in service, controller, and routes

--- infrastructure/database/repository/menu.go
+++ infrastructure/database/repository/menu.go
@@ -87,3 +87,30 @@ func (r *menuRepository) GetMenusByCategoryID(ctx context.Context, tx interface{
 
 	return menuEntities, nil
 }
+
+func (r *menuRepository) UpdateMenuAvailability(ctx context.Context, tx interface{}, id string, isAvailable bool) (menu.Menu, error) {
+	validatedTransaction, err := validation.ValidateTransaction(tx)
+	if err != nil {
+		return menu.Menu{}, err
+	}
+
+	db := validatedTransaction.DB()
+	if db == nil {
+		db = r.db.DB()
+	}
+
+	var menuSchema schema.Menu
+
+	if err = db.WithContext(ctx).Where("id = ?", id).Take(&menuSchema).Error; err != nil {
+		return menu.Menu{}, err
+	}
+
+	menuSchema.IsAvailable = isAvailable
+
+	if err = db.WithContext(ctx).Save(&menuSchema).Error; err != nil {
+		return menu.Menu{}, err
+	}
+
+	menuEntity := schema.MenuSchemaToEntity(menuSchema)
+	return menuEntity, nil
+}
