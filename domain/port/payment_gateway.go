9f71e4510dce3e0c0a304e40f338a4be32862f1c
fawwasaldy <fawwas.aldy2111@gmail.com>
2025-06-29 21:55:34 +0700

    feat: update ProcessPayment to return structured response with token and payment link

--- domain/port/payment_gateway.go
+++ domain/port/payment_gateway.go
@@ -8,7 +8,12 @@ import (
 
 type (
 	PaymentGatewayPort interface {
-		ProcessPayment(ctx context.Context, tx interface{}, transactionEntity transaction.Transaction) (string, error)
+		ProcessPayment(ctx context.Context, tx interface{}, transactionEntity transaction.Transaction) (ProcessPaymentResponse, error)
 		HookPayment(ctx context.Context, tx interface{}, transactionId uuid.UUID, datas map[string]interface{}) error
+	}
+
+	ProcessPaymentResponse struct {
+		Token       string
+		PaymentLink string
 	}
 )
