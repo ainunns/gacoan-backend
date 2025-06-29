15b03a5dd588845565e4e740c7ce6c977956763f
fawwasaldy <fawwas.aldy2111@gmail.com>
2025-06-30 01:33:39 +0700

    feat: add authorization middleware for transaction routes based on user roles

--- presentation/route/transaction.go
+++ presentation/route/transaction.go
@@ -2,28 +2,72 @@ package route
 
 import (
 	"fp-kpl/application/service"
+	"fp-kpl/domain/user"
 	"fp-kpl/presentation/controller"
 	"fp-kpl/presentation/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
-func TransactionRoute(route *gin.Engine, transactionController controller.TransactionController, jwtService service.JWTService) {
+func TransactionRoute(route *gin.Engine, transactionController controller.TransactionController, jwtService service.JWTService, userService service.UserService) {
 	transactionGroup := route.Group("/api/transaction")
 	{
-		transactionGroup.POST("/", middleware.Authenticate(jwtService), transactionController.CreateTransaction)
+		transactionGroup.POST("/",
+			middleware.Authenticate(jwtService),
+			middleware.Authorize(userService, []user.Role{
+				{user.RoleCustomer},
+				{user.RoleWaiter},
+				{user.RoleSuperAdmin},
+			}),
+			transactionController.CreateTransaction)
 		transactionGroup.GET("/", middleware.Authenticate(jwtService), transactionController.GetAllTransactionsWithPagination)
 		transactionGroup.GET("/:id", middleware.Authenticate(jwtService), transactionController.GetTransactionByID)
 		transactionGroup.POST("/hook", transactionController.HookTransaction)
 
 		// Kitchen
-		transactionGroup.GET("/next-order", middleware.Authenticate(jwtService), transactionController.GetNextOrder)
-		transactionGroup.POST("/start-cooking", middleware.Authenticate(jwtService), transactionController.StartCooking)
-		transactionGroup.POST("/finish-cooking", middleware.Authenticate(jwtService), transactionController.FinishCooking)
+		transactionGroup.GET("/next-order",
+			middleware.Authenticate(jwtService),
+			middleware.Authorize(userService, []user.Role{
+				{user.RoleKitchen},
+				{user.RoleSuperAdmin},
+			}),
+			transactionController.GetNextOrder)
+		transactionGroup.POST("/start-cooking",
+			middleware.Authenticate(jwtService),
+			middleware.Authorize(userService, []user.Role{
+				{user.RoleKitchen},
+				{user.RoleSuperAdmin},
+			}),
+			transactionController.StartCooking)
+		transactionGroup.POST("/finish-cooking",
+			middleware.Authenticate(jwtService),
+			middleware.Authorize(userService, []user.Role{
+				{user.RoleKitchen},
+				{user.RoleSuperAdmin},
+			}),
+			transactionController.FinishCooking)
 
 		// Waiter
-		transactionGroup.GET("/ready-to-serve", middleware.Authenticate(jwtService), transactionController.GetAllReadyToServeTransactionList)
-		transactionGroup.POST("/start-delivering", middleware.Authenticate(jwtService), transactionController.StartDelivering)
-		transactionGroup.POST("/finish-delivering", middleware.Authenticate(jwtService), transactionController.FinishDelivering)
+		transactionGroup.GET("/ready-to-serve",
+			middleware.Authenticate(jwtService),
+			middleware.Authorize(userService, []user.Role{
+				{user.RoleWaiter},
+				{user.RoleSuperAdmin},
+			}),
+			transactionController.GetAllReadyToServeTransactionList)
+		transactionGroup.POST("/start-delivering",
+			middleware.Authenticate(jwtService),
+			middleware.Authorize(userService, []user.Role{
+				{user.RoleWaiter},
+				{user.RoleSuperAdmin},
+			}),
+			transactionController.StartDelivering)
+		transactionGroup.POST("/finish-delivering",
+			middleware.Authenticate(jwtService),
+			middleware.Authorize(userService, []user.Role{
+				{user.RoleWaiter},
+				{user.RoleSuperAdmin},
+			}),
+			transactionController.FinishDelivering)
 	}
 }
